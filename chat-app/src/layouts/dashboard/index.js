e22934087a50432f18e1ef4f0dbf17f7c36b7402
Niranjan <niranjanverma823@gmail.com>
2024-03-18 03:07:40 +0530

    toggle button and user icon added

--- chat-app/src/layouts/dashboard/index.js
+++ chat-app/src/layouts/dashboard/index.js
@@ -1,17 +1,67 @@
 import { useTheme } from "@emotion/react";
-import { Box, Divider, IconButton, Stack } from "@mui/material";
+import { Avatar, Box, Divider, IconButton, Stack, Switch } from "@mui/material";
 import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
-
 import Logo from "../../assets/Images/logo.ico";
 import { Nav_Buttons } from "../../data/";
 import { Gear } from "phosphor-react";
+import { faker } from "@faker-js/faker";
+import { styled } from "@mui/material";
+
+import useSettings from "../../hooks/useSettings";
+
+const AntSwitch = styled(Switch)(({ theme }) => ({
+  width: 40,
+  height: 20,
+  padding: 0,
+  display: "flex",
+  "&:active": {
+    "& .MuiSwitch-thumb": {
+      width: 15,
+    },
+    "& .MuiSwitch-switchBase.Mui-checked": {
+      transform: "translateX(9px)",
+    },
+  },
+  "& .MuiSwitch-switchBase": {
+    padding: 2,
+    "&.Mui-checked": {
+      transform: "translateX(20px)",
+      color: "#fff",
+      "& + .MuiSwitch-track": {
+        opacity: 1,
+        backgroundColor: theme.palette.mode === "dark" ? "#177ddc" : "#1890ff",
+      },
+    },
+  },
+  "& .MuiSwitch-thumb": {
+    boxShadow: "0 2px 4px 0 rgb(0 35 11 / 20%)",
+    width: 16,
+    height: 16,
+    borderRadius: 8,
+    transition: theme.transitions.create(["width"], {
+      duration: 200,
+    }),
+  },
+  "& .MuiSwitch-track": {
+    borderRadius: 20 / 2,
+    opacity: 1,
+    backgroundColor:
+      theme.palette.mode === "dark"
+        ? "rgba(255,255,255,.35)"
+        : "rgba(0,0,0,.25)",
+    boxSizing: "border-box",
+  },
+}));
 
 const DashboardLayout = () => {
   const [selected, setSelected] = useState(0);
 
   const theme = useTheme();
   console.log(theme);
+
+  const { onToggleMode } = useSettings();
+
   return (
     <>
       <Box
@@ -26,29 +76,65 @@ const DashboardLayout = () => {
         <Stack
           direction="column"
           alignItems="center"
-          sx={{ width: "100%" }}
+          justifyContent="space-between"
+          sx={{ height: "100%" }}
           spacing={3}
         >
-          <Box
-            sx={{
-              backgroundColor: theme.palette.primary.main,
-              height: 64,
-              width: 64,
-              borderRadius: 1.5,
-            }}
-          >
-            <img src={Logo} alt="chat app logo" />
-          </Box>
-          <Stack
-            sx={{
-              width: "max-content",
-              direction: "column",
-              alignItems: "center",
-            }}
-            spacing={3}
-          >
-            {Nav_Buttons.map((el) =>
-              el.index === selected ? (
+          <Stack alignItems={"center"} spacing={4}>
+            <Box
+              sx={{
+                backgroundColor: theme.palette.primary.main,
+                height: 64,
+                width: 64,
+                borderRadius: 1.5,
+              }}
+            >
+              <img src={Logo} alt="chat app logo" />
+            </Box>
+            <Stack
+              sx={{
+                width: "max-content",
+                direction: "column",
+                alignItems: "center",
+              }}
+              spacing={3}
+            >
+              {Nav_Buttons.map((el) =>
+                el.index === selected ? (
+                  <Box
+                    p={1}
+                    sx={{
+                      backgroundColor: theme.palette.primary.main,
+                      borderRadius: 1.5,
+                    }}
+                  >
+                    <IconButton
+                      sx={{ width: "max-content", color: "#fff" }}
+                      key={el.index}
+                    >
+                      {el.icon}
+                    </IconButton>
+                  </Box>
+                ) : (
+                  <IconButton
+                    onClick={() => {
+                      setSelected(el.index);
+                    }}
+                    sx={{
+                      width: "max-content",
+                      color:
+                        theme.palette.mode === "light"
+                          ? "#000"
+                          : theme.palette.text.primary,
+                    }}
+                    key={el.index}
+                  >
+                    {el.icon}
+                  </IconButton>
+                )
+              )}
+              <Divider sx={{ width: "48px" }} />
+              {selected === 3 ? (
                 <Box
                   p={1}
                   sx={{
@@ -56,48 +142,36 @@ const DashboardLayout = () => {
                     borderRadius: 1.5,
                   }}
                 >
-                  <IconButton
-                    sx={{ width: "max-content", color: "#fff" }}
-                    key={el.index}
-                  >
-                    {el.icon}
+                  <IconButton sx={{ width: "max-content", color: "#fff" }}>
+                    <Gear />
                   </IconButton>
                 </Box>
               ) : (
                 <IconButton
-                  onClick={() => {
-                    setSelected(el.index);
+                  sx={{
+                    width: "max-content",
+                    color:
+                      theme.palette.mode === "light"
+                        ? "#000"
+                        : theme.palette.text.primary,
                   }}
-                  sx={{ width: "max-content", color: "#000" }}
-                  key={el.index}
+                  onClick={() => {
+                    setSelected(3);
+                  }}
                 >
-                  {el.icon}
-                </IconButton>
-              )
-            )}
-            <Divider sx={{ width: "48px" }} />
-            {selected === 3 ? (
-              <Box
-                p={1}
-                sx={{
-                  backgroundColor: theme.palette.primary.main,
-                  borderRadius: 1.5,
-                }}
-              >
-                <IconButton sx={{ width: "max-content", color: "#fff" }}>
                   <Gear />
                 </IconButton>
-              </Box>
-            ) : (
-              <IconButton
-                sx={{ width: "max-content", color: "#000" }}
-                onClick={() => {
-                  setSelected(3);
-                }}
-              >
-                <Gear />
-              </IconButton>
-            )}
+              )}
+            </Stack>
+          </Stack>
+          <Stack spacing={4}>
+            <AntSwitch
+              onChange={() => {
+                onToggleMode();
+              }}
+              defaultChecked
+            />
+            <Avatar src={faker.image.avatar()} />
           </Stack>
         </Stack>
       </Box>

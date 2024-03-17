5cdd06dd53ff7a9a5525782b3dc89de9a27cef95
Niranjan <niranjanverma823@gmail.com>
2024-03-18 02:11:31 +0530

    first logo added

--- chat-app/src/layouts/dashboard/index.js
+++ chat-app/src/layouts/dashboard/index.js
@@ -1,11 +1,37 @@
+import { useTheme } from "@emotion/react";
+import { Box, Stack } from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
 
-const DashboardLayout = () => {
+import Logo from "../../assets/Images/logo.ico";
 
+const DashboardLayout = () => {
+  const theme = useTheme();
+  console.log(theme);
   return (
     <>
-      Dashboard Layout
+      <Box
+        sx={{
+          backgroundColor: theme.palette.background.paper,
+          boxShadow: "0px 0px 2px rgba(0,0,0,0.25)",
+          height: "100vh",
+          width: 100,
+          padding: 2,
+        }}
+      >
+        <Stack direction="column" alignItems="center" sx={{ width: "100%" }}>
+          <Box
+            sx={{
+              backgroundColor: theme.palette.primary.main,
+              height: 64,
+              width: 64,
+              borderRadius: 1.5,
+            }}
+          >
+            <img src={Logo} alt="chat app logo" />
+          </Box>
+        </Stack>
+      </Box>
       <Outlet />
     </>
   );
